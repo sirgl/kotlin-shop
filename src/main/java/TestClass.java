f9027ffdc3246cdc4560e244f60c659c2d2165db
roman <rom6190@yandex.ru>
2016-12-05 13:47:32 +0700

    add test file for javadoc

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,13 @@
+/**
+ * Created by roman on 05.12.16.
+ */
+public class TestClass {
+    /**
+     * adasdadsasddshkj dsflkjdslfkjs
+     *  sdfsdfsdfdsf sdf sdf sdfdsf
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
